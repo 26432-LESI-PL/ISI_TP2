9295b9297f1f3048b28d967d2dbc05f5e7a3c796
danielalexis <danielalexis.o.pereira@gmail.com>
2024-12-20 20:14:52 +0000

    Add Terra service

---
+++ Terra/ServiceDataContract.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Terra {
+    // Data Contract for Beneficiario
+    [DataContract]
+    public class Beneficiario
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string NomeRepresentante { get; set; }
+
+        [DataMember]
+        public string Contacto { get; set; }
+
+        [DataMember]
+        public string Nacionalidade { get; set; } = "Português";
+
+        [DataMember]
+        public int DimensaoAgregado { get; set; }
+    }
+
+    // Data Contract for Turno
+    [DataContract]
+    public class Turno
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public DateTime Time { get; set; }
+
+        [DataMember]
+        public string Tarefas { get; set; }
+    }
+
+    // Service Contract
+    [ServiceContract]
+    public interface IDoaMaisTerraService
+    {
+        // Beneficiario Operations
+        [OperationContract]
+        Beneficiario GetBeneficiarioById(int id);
+
+        [OperationContract]
+        IEnumerable<Beneficiario> GetAllBeneficiarios();
+
+        [OperationContract]
+        void AddBeneficiario(Beneficiario beneficiario);
+
+        [OperationContract]
+        void UpdateBeneficiario(Beneficiario beneficiario);
+
+        [OperationContract]
+        void DeleteBeneficiario(int id);
+
+        // Turno Operations
+        [OperationContract]
+        Turno GetTurnoById(int id);
+
+        [OperationContract]
+        IEnumerable<Turno> GetAllTurnos();
+
+        [OperationContract]
+        void AddTurno(Turno turno);
+
+        [OperationContract]
+        void UpdateTurno(Turno turno);
+
+        [OperationContract]
+        void DeleteTurno(int id);
+    }
+}
+
---
+++ Terra/Services/BeneficiarioService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Terra.Services
+{
+    public class BeneficiarioTurnoService : IDoaMaisTerraService
+    {
+        public void AddBeneficiario(Beneficiario beneficiario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBeneficiario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBeneficiario(Beneficiario beneficiario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Beneficiario> GetAllBeneficiarios()
+        {
+            return
+            [
+                new() {
+                    Id = 1,
+                    NomeRepresentante = "João",
+                    Contacto = "912345678",
+                    DimensaoAgregado = 3
+                },
+                new() {
+                    Id = 2,
+                    NomeRepresentante = "Maria",
+                    Contacto = "912345678",
+                    DimensaoAgregado = 4
+                }
+            ];
+        }
+
+        public Beneficiario GetBeneficiarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTurno(Turno turno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTurno(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Turno> GetAllTurnos()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Turno GetTurnoById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void UpdateTurno(Turno turno)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
